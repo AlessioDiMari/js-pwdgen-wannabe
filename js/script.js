33fa19e49e8bcca057f1a0a44e002d7025f4298f
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-01-25 18:04:36 +0100

    richiesta variabili tramite prompt

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// dichiarazione delle variabili tramite prompt
+
+let firstname = prompt("Qual é il tuo nome?");
+
+let lastname = prompt("Qual é il tuo cognome?");
+
+let color = prompt("Qual é il tuo colore preferito?");
+
+let number = prompt("Qual é il tuo numero preferito?");
